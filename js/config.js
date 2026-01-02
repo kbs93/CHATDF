0085a7ecfc19d38cf06478135d4b5f20b1023a27
kbs93 <kbswebb@gmail.com>
2026-01-02 17:09:13 -0300

    arq  config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA5ApoFDkyW9nyxrgCjzbWGiuAwP2ldUD0",
+  authDomain: "chatdf-4102025.firebaseapp.com",
+  projectId: "chatdf-4102025",
+  storageBucket: "chatdf-4102025.appspot.com", // ✅ corrigido (.appspot.com)
+  messagingSenderId: "74233540933",
+  appId: "1:74233540933:web:df0e118e40c1e1513fce2c",
+  measurementId: "G-1N8ZP3MK3N",
+  
+};
