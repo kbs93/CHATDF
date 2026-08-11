b406ea167a8ddadba07657b0233fd2976501c313
kbs93 <kbswebb@gmail.com>
2026-08-11 20:58:06 -0300

    nova melhoria no DOM 11-08-26

--- js/messages.js
+++ js/messages.js
@@ -1271,7 +1271,7 @@ export async function sendMessage(input) {
     return;
   }
 
-  const perfilRef = doc(db, "salas", "users", currentUser.uid);
+  const perfilRef = doc(db, "users", currentUser.uid);
   const perfilSnap = await getDoc(perfilRef);
   const userProfile = perfilSnap.exists() ? perfilSnap.data() : {};
 
